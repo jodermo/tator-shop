45ad7bdec7fac2fc3afe098562f542b28e50bd22
Moritz Petzka <moritz@petzka.com>
2020-10-05 10:45:47 +0200

    CLI update

--- tator-shop.module.ts
+++ tator-shop.module.ts
@@ -4,7 +4,6 @@ import { ProductOverviewComponent } from './components/backend/product-overview/
 import { CartComponent } from './components/cart/cart.component';
 import { ShopConfigurationComponent } from './components/backend/shop-configuration/shop-configuration.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
-import { TatorImportsModule } from '../../tator-app/angular-app/src/app/tator-imports.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CashRegisterBarcodeComponent } from './components/backend/cash-register/cach-register-barcode/cash-register-barcode.component';
 import { CashRegisterComponent } from './components/backend/cash-register/cash-register.component';
@@ -13,15 +12,10 @@ import { CheckoutTableComponent } from './components/backend/checkout-overview/c
 import { CheckoutOverviewComponent } from './components/backend/checkout-overview/checkout-overview.component';
 import { CashRegisterSettingsComponent } from './components/backend/cash-register-settings/cash-register-settings.component';
 import { IndividualProductComponent } from './components/backend/cash-register/individual-product/individual-product.component';
-import { TatorUiComponentsModule } from '../../tator-app/angular-app/src/app/modules/tator-core/tator-ui-components.module';
-import { TatorFileComponentsModule } from '../../tator-app/angular-app/src/app/modules/tator-core/tator-file-components.module';
-import { TatorFormComponentsModule } from '../../tator-app/angular-app/src/app/modules/tator-core/tator-form-components.module';
-import { TatorUserComponentsModule } from '../../tator-app/angular-app/src/app/modules/tator-core/tator-user-components.module';
-import { TatorDataComponentsModule } from '../../tator-app/angular-app/src/app/modules/tator-core/tator-data-components.module';
 import { CashRegisterBarcodeDataComponent } from './components/backend/cash-register/cach-register-barcode-data/cash-register-barcode-data.component';
-import { FlatpickrModule } from 'angularx-flatpickr';
 import { PurchaseComponent } from './components/backend/purchase/purchase.component';
 import { CustomerDisplayComponent } from './components/backend/customer-display/customer-display.component';
+import { ExtensionImportsModule } from '../../tator-app/angular-app/src/app/extension-imports.module';
 
 @NgModule({
     declarations: [
@@ -43,13 +37,7 @@ import { CustomerDisplayComponent } from './components/backend/customer-display/
         CustomerDisplayComponent
     ],
     imports: [
-        TatorImportsModule,
-        TatorDataComponentsModule,
-        TatorFileComponentsModule,
-        TatorUiComponentsModule,
-        TatorFormComponentsModule,
-        TatorUserComponentsModule,
-        FlatpickrModule,
+        ExtensionImportsModule
     ],
     providers: [],
     exports: [
